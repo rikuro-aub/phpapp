76b863962cdda3dd3fe732c22cbbfdd3a9fe11b9
3no3 <ulis7524@gmail.com>
2020-03-25 15:21:06 +0900

    Revert "ルーティングの基本設定を追加"
    This reverts commit 5908e36354c062157ac728d6594af85274e9dc50.

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
-Route::get('/{any}', 'AppController@index')->where('any', '.*');
+Route::get('/home', 'HomeController@index')->name('home');
